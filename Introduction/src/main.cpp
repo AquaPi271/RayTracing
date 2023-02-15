5779c37231c9644269d65c309e87600761ab5913
AquaPi271 <tozimek@gmail.com>
2023-02-15 03:23:37 +0000

    Clang formatting added.

--- Introduction/src/main.cpp
+++ Introduction/src/main.cpp
@@ -1,37 +1,40 @@
 #include "baseline_png_config.h"
 #include <iostream>
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
+int main(int argc, char **argv)
+{
+    // CMakefile version info example:
+
     // report version
-    std::cout << argv[0] << " Version " << baseline_png_VERSION_MAJOR << "."
-              << baseline_png_VERSION_MINOR << std::endl;
-    std::cout << "Usage: " << argv[0] << " number" << std::endl;
-    return 1;
-  }
+    //    std::cout << argv[0] << " Version " << baseline_png_VERSION_MAJOR << "."
+    //              << baseline_png_VERSION_MINOR << std::endl;
+    //    std::cout << "Usage: " << argv[0] << " number" << std::endl;
 
-  // Image
+    // Image
 
-  const int image_width = 256;
-  const int image_height = 256;
+    const int image_width  = 256;
+    const int image_height = 256;
 
-  // Render
+    // Render
 
-  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
-  for (int j = image_height - 1; j >= 0; --j) {
-    for (int i = 0; i < image_width; ++i) {
-      auto r = double(i) / (image_width - 1);
-      auto g = double(j) / (image_height - 1);
-      auto b = 0.25;
+    for (int j = image_height - 1; j >= 0; --j)
+    {
+        for (int i = 0; i < image_width; ++i)
+        {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.25;
 
-      int ir = static_cast<int>(255.999 * r);
-      int ig = static_cast<int>(255.999 * g);
-      int ib = static_cast<int>(255.999 * b);
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
 
-      std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+            std::cout << ir << ' ' << ig << ' ' << ib << "\n";
+        }
     }
-  }
 
-  return 0;
+    return 0;
 }
